ae77a0ae8d6cbd4377a19c36b047fd6e668ce442
James Whitacre <james.whitacre@gmail.com>
2021-03-03 00:06:55 -0500

    Final Updates
    Final Updates

--- Scripts/ClassifyPAMAPLASDataset.py
+++ Scripts/ClassifyPAMAPLASDataset.py
@@ -27,7 +27,6 @@ Licence:    This code may not be distributed.
 
 ''' Import Modules '''
 import arcpy
-from datetime import datetime
 import lidarlas
 import multiprocessing
 from multiprocessing import Pool, cpu_count
@@ -49,62 +48,28 @@ input_las_sr = arcpy.GetParameter(2)
 compute_stats = arcpy.GetParameterAsText(3)
 
 # Group: Change Classification Options
-# Change Original Class Codes (Value Table: Current Class > Long | New Class > Long | Synthetic > String | KeyPoint > String | Withheld > String | Overlap > String | Class Code Filter > String; Optional; Filter: Current Class, New Class > Range 0 - 255 | Synthetic, KeyPoint, Withheld, Overlap > NO_CHANGE, SET, CLEAR; Default: 8 8 NO_CHANGE SET NO_CHANGE NO_CHANGE)
-change_original_codes = arcpy.GetParameterAsText(4)
 # Classification Feature Classes (Value Table: Features > Feature Layer | Buffer Distance > Double | New Class > Long | Synthetic > String | KeyPoint > String | Withheld > String | Overlap > String | Class Code Filter > String; Optional; Filter: Feature > Point, Polyline, Polygon | New Class > Range 0 - 255 | Synthetic, KeyPoint, Withheld, Overlap > NO_CHANGE, SET, CLEAR; Validation: Create Error Message if Class Code Filter contians text other than integer between 0 and 255 or a comma)
-feature_classes = arcpy.GetParameterAsText(5).split(';') if arcpy.GetParameterAsText(5) else []
+feature_classes = arcpy.GetParameterAsText(4).split(';')
 
 # Group: Ground Classification Options
 # Ground Detection Method (String; Optional; Filter: STANDARD, CONSERVATIVE, AGGRESSIVE)
-ground_method = arcpy.GetParameterAsText(6)
+ground_method = arcpy.GetParameterAsText(5)
 # Reuse Existing Ground (Boolean; Optional; Filter: True = REUSE_GROUND, False = RECLASSIFY_GROUND; Default: true; Validation: Enable if Ground Detection Method is not empty)
-reuse_ground = arcpy.GetParameterAsText(7)
+reuse_ground = arcpy.GetParameterAsText(6)
 # DEM Resolution (Linear Unit; Optional; Validation: Enable if Ground Detection Method is not empty)
-dem_resolution = arcpy.GetParameterAsText(8)
-
-# Group: Building Classification Options
-# Classify Building Points (String; Optional; Filter: RECLASSIFY_BUILDING, REUSE_BUILDING)
-classify_buildings = arcpy.GetParameterAsText(9)
-# Minimum Rooftop Height (Linear Unit; Optional; Filter: Meters, Feet; Validation: Enable if Classify Building Points is not empty, Set default value of 2 Meters (6.562 Feet) based on input spatial reference, Create Error Message if Minimum Rooftop Height is enabled and empty)
-min_roof_height = arcpy.GetParameterAsText(10)
-# Minimum Building Area (Areal Unit; Optional; Filter: Square Meters, Square Feet; Validation: Enable if Classify Building Points is not empty, Set default value of 10 Square Meters (107.639 Square Feet) based on input spatial reference, Create Error Message if Minimum Building Area is enabled and empty)
-min_building_area = arcpy.GetParameterAsText(11)
-# Is Photogrammetric Data (Boolean; Optional; Filter: True = PHOTOGRAMMETRIC_DATA, False = NOT_PHOTOGRAMMETRIC_DATA;Validation: Enable if Classify Building Points is not empty)
-photogrammetric_data = arcpy.GetParameterAsText(12)
-
-# Group: Vegetation Classification Options
-# Vegetation Classification Method (String; Optional; Filter: EXISTING_VEGETATION, NEW_VEGETATION, ALL_VEGETATION)
-vegetation_method = arcpy.GetParameterAsText(13)
-# Exisiting Vegetation Classification Codes (Long, Multiple values; Optional; Filter: Range 0 - 255; Validation: Enable if Vegetation Classification Method is EXISTING_VEGETATION or ALL_VEGETATION, Create Error Message if Exisiting Vegetation Classification Codes is enabled and empty)
-exist_veg_codes = arcpy.GetParameterAsText(14).split(';')
-# Height Classification (Value Table: Class Code > Long | Height > Double; ; Filter: Class Code > Range 0 - 255; Validataion: Enable if Vegetation Classification Method is not empty, Set default values based on input spatial reference unit of '1 0.5;3 2;4 5;5 65' for meter and '1 1.64;3 6.562;4 16.404;5 213.255' for feet, Create Error Message if Height Classification is enabled and empty)
-height_classification = arcpy.GetParameterAsText(15)
-# Vegetation Area Cell Size (Double; Optional; Validation: Enable if Vegetation Classification Method is not empty, Set default values based on input spatial reference unit of 5 for meter and 16.404 for feet, Create Error Message if Vegetation Area Cell Size is enabled and empty AND if Classify Building Points is not empty OR Output Vegetation Areas Feature Class is not empty)
-vegetation_cell_size = arcpy.GetParameter(16)
-
-# Group: Noise Classification Options
-# Noise Classification Method (String; Optional; Filter: ALL_NOISE, HIGH_NOISE, NONE; Default: ALL_NOISE)
-noise_method = arcpy.GetParameterAsText(17)
-# Noise Height From Ground (Double; Optional; Validation: Enable if Noise Classification is ALL_NOISE or HIGH_NOISE, Set default value based on input spatial reference unit of 65 for meter or 213.255 for feet, Create Error Message if Height Classification is enabled and empty)
-noise_height = arcpy.GetParameterAsText(18)
-# Noise Neighborhood Point Limit (Long; Optional; Validation: Enable if Noise Classification is ALL_NOISE, Set default value as 25, Create Error Message if Height Classification is enabled and empty)
-max_neighbors = arcpy.GetParameterAsText(19)
-# Noise Neighborhood Width (Linear Unit; Optional; Filter: Meters, Feet; Validation: Enable if Noise Classification is ALL_NOISE, Set default value based on input spatial reference unit of 10 Meters or 32.808 for feet, Create Error Message if Height Classification is enabled and empty)
-step_width = arcpy.GetParameterAsText(20)
-# Noise Neighborhood Height (Linear Unit; Optional; Filter: Meters, Feet; Validation: Enable if Noise Classification is ALL_NOISE, Set default value based on input spatial reference unit of 10 Meters or 32.808 for feet, Create Error Message if Height Classification is enabled and empty)
-step_height = arcpy.GetParameterAsText(21)
+dem_resolution = arcpy.GetParameterAsText(7)
 
 # Group: Processing Extent
 # Processing Extent (Extent; Optional; Default: DEFAULT)
-extent = arcpy.GetParameterAsText(22)
+extent = arcpy.GetParameterAsText(8)
 # Processing Boundary (Feature Set; Optional)
-boundary = arcpy.GetParameterAsText(23)
+boundary = arcpy.GetParameterAsText(9)
 # Process Entire LAS Files That Intersect Extent (Boolean; Optional; Filter: True = PROCESS_ENTIRE_FILES, False = PROCESS_EXTENT )
-process_entire_files = arcpy.GetParameterAsText(24)
+process_entire_files = arcpy.GetParameterAsText(10)
 
 # Group: Output Options
 # Output LAS Dataset (LAS Dataset; Optional; Output)
-out_las_dataset = arcpy.GetParameterAsText(25)
+out_las_dataset = arcpy.GetParameterAsText(11)
 
 
 ''' Script '''
@@ -134,44 +99,6 @@ for i in input_files:
 cpu_ct = cpu_count() if len(input_las_files) > cpu_count() else len(input_las_files)
 arcpy.AddMessage(f'Total CPU Count: {cpu_count()} | File Count: {len(input_las_files)} | CPUs Used: {cpu_ct}')
 
-# Create a temporary ArcGIS LAS Dataset of all LAS files
-lasd_temp = arcpy.CreateUniqueName(os.path.join(scratch_folder, 'LASD_Temp.lasd'))
-arcpy.management.CreateLasDataset(input_files, lasd_temp, folder_recursion, None, input_las_sr, compute_stats, 'RELATIVE_PATHS', 'NO_FILES')
-
-# Change Original Class Codes
-if change_original_codes:
-    arcpy.SetProgressorLabel('Changing Original Class Codes...')
-    change_codes_arg = [change_original_codes]
-    change_codes_args = [[las_file] + change_codes_arg + global_args for las_file in input_las_files]
-    with Pool(processes=cpu_ct) as pool:
-        results = pool.starmap(lidarlas.ChangeLasClassCodes, change_codes_args)
-
-# Reclassify Exisiting Vegetation Points
-if exist_veg_codes and vegetation_method in ['EXISTING_VEGETATION', 'ALL_VEGETATION']:
-    arcpy.SetProgressorLabel('Reclassifying Exisitng Vegetation Points...')
-    # Change Unclassified and Unassigned Point Codes
-    change_codes_arg = ['0 30 NO_CHANGE NO_CHANGE NO_CHANGE NO_CHANGE;1 31 NO_CHANGE NO_CHANGE NO_CHANGE NO_CHANGE']
-    change_codes_args = [[las_file] + change_codes_arg + global_args for las_file in input_las_files]
-    with Pool(processes=cpu_ct) as pool:
-        results = pool.starmap(lidarlas.ChangeLasClassCodes, change_codes_args)
-
-    # Change Existing Vegetation Points to Unclassified
-    change_codes_arg = [';'.join([f'{veg_code} 0 NO_CHANGE NO_CHANGE NO_CHANGE NO_CHANGE' for veg_code in exist_veg_codes])]
-    change_codes_args = [[las_file] + change_codes_arg + global_args for las_file in input_las_files]
-    with Pool(processes=cpu_ct) as pool:
-        results = pool.starmap(lidarlas.ChangeLasClassCodes, change_codes_args)
-
-    # Classify Existing Vegetation to Vegetation Levels by Height 
-    height_arg = ['GROUND', height_classification, noise_method]
-    height_args = [[las_file] + height_arg + global_args for las_file in input_las_files]
-    with Pool(processes=cpu_ct) as pool:
-        results = pool.starmap(lidarlas.ClassifyLasByHeight, height_args)
-
-    # Change Back Unclassified and Unassigned Point Codes and Flag Noise Points
-    change_codes_arg = ['30 0 NO_CHANGE NO_CHANGE NO_CHANGE NO_CHANGE;31 1 NO_CHANGE NO_CHANGE NO_CHANGE NO_CHANGE;7 7 NO_CHANGE NO_CHANGE SET NO_CHANGE;18 18 NO_CHANGE NO_CHANGE SET NO_CHANGE']
-    change_codes_args = [[las_file] + change_codes_arg + global_args for las_file in input_las_files]
-    with Pool(processes=cpu_ct) as pool:
-        results = pool.starmap(lidarlas.ChangeLasClassCodes, change_codes_args)
 
 # Classify Ground Points
 if ground_method == 'AGGRESSIVE':
@@ -206,106 +133,6 @@ if feature_classes:
         with Pool(processes=cpu_ct) as pool:
             results = pool.starmap(lidarlas.SetLasClassCodesUsingFeaturesFiltered, classify_features_args)
 
-# Classify Noise Points
-# Classify High Noise Points
-if noise_height:
-    arcpy.SetProgressorLabel('Classifying High Noise Points...')
-    height_arg = ['GROUND', f'1 {noise_height}', 'HIGH_NOISE']
-    height_args = [[las_file] + height_arg + global_args for las_file in input_las_files]
-    with Pool(processes=cpu_ct) as pool:
-        results = pool.starmap(lidarlas.ClassifyLasByHeight, height_args)
-
-# Classify Other Noise Points
-if noise_method == 'ALL_NOISE':
-    arcpy.SetProgressorLabel('Classifying Other Noise Points...')
-    noise_arg = ['ISOLATION', 'CLASSIFY', 'WITHHELD', None, None, None, max_neighbors, step_width, step_height, None, compute_stats, extent, process_entire_files]
-    noise_args = [[las_file] + noise_arg for las_file in input_las_files]
-    with Pool(processes=cpu_ct) as pool:
-        results = pool.starmap(lidarlas.ClassifyLasNoise, noise_args)
-
-# Classify Building Points (Not Multiprocessing; it's fast)
-if classify_buildings:
-    arcpy.SetProgressorLabel('Classifying Building Points...')
-    arcpy.ddd.ClassifyLasBuilding(lasd_temp, min_roof_height, min_building_area, compute_stats, extent, boundary, process_entire_files, None, classify_buildings, photogrammetric_data)
-
-# Classify New Vegetation Points
-if vegetation_method in ['NEW_VEGETATION', 'ALL_VEGETATION']:
-    arcpy.SetProgressorLabel('Classifying New Vegetation Points...')
-    # Classify Unclassified and Unassigned Points to Vegetation Levels
-    height_arg = ['GROUND', height_classification, noise_method]
-    height_args = [[las_file] + height_arg + global_args for las_file in input_las_files]
-    with Pool(processes=cpu_ct) as pool:
-        results = pool.starmap(lidarlas.ClassifyLasByHeight, height_args)
-
-# Refine Vegetation and Building Areas
-if classify_buildings and vegetation_cell_size:
-    arcpy.SetProgressorLabel('Refining Building and Vegetation Points...')
-    # Create All, Vegetation, and Building Points Count Rasters
-    arcpy.SetProgressorLabel('Refining Building and Vegetation Points: Counting Points...')
-    all_point_count = arcpy.CreateUniqueName(os.path.join(scratch_folder, 'All_Point_Count.tif'))
-    veg_codes = '3;4;5'
-    veg_point_count = arcpy.CreateUniqueName(os.path.join(scratch_folder, 'Veg_Point_Count.tif'))
-    building_codes = '6'
-    building_point_count = arcpy.CreateUniqueName(os.path.join(scratch_folder, 'Building_Point_Count.tif'))
-
-    las_stats_args = [[lasd_temp, all_point_count, 'POINT_COUNT', 'CELLSIZE', vegetation_cell_size], [lasd_temp, veg_point_count, 'POINT_COUNT', 'CELLSIZE', vegetation_cell_size, veg_codes], [lasd_temp, building_point_count, 'POINT_COUNT', 'CELLSIZE', vegetation_cell_size, building_codes]]
-    
-    with Pool(processes=4) as pool:
-        results = pool.starmap(lidarlas.LasPointStatsAsRaster, las_stats_args)
-
-    # Reclassify and Smooth All, Vegetation, and Building Points Count Rasters
-    arcpy.SetProgressorLabel('Refining Building and Vegetation Points: Smoothing Rasters...')
-
-    all_point_count_smooth = arcpy.CreateUniqueName(os.path.join(scratch_folder, 'All_Point_Count_Smooth.tif'))
-    veg_point_count_smooth = arcpy.CreateUniqueName(os.path.join(scratch_folder, 'Veg_Point_Count_Smooth.tif'))
-    building_point_count_smooth = arcpy.CreateUniqueName(os.path.join(scratch_folder, 'Building_Point_Count_Smooth.tif'))
-
-    reclass_smooth_args = [[all_point_count, all_point_count_smooth], [veg_point_count, veg_point_count_smooth], [building_point_count, building_point_count_smooth]]
-    
-    with Pool(processes=4) as pool:
-        results = pool.starmap(lidarlas.ReclassSmooth, reclass_smooth_args)
-
-    # Calcaulte Vegetated Weighted Density (Not Multiprocessing)
-    arcpy.SetProgressorLabel('Refining Building and Vegetation Points: Calcaulting Density...')
-    veg_density = arcpy.sa.Minus(arcpy.sa.Divide(veg_point_count_smooth, all_point_count_smooth), arcpy.sa.Divide(building_point_count_smooth, all_point_count_smooth))
-    veg_density.save(arcpy.CreateUniqueName(os.path.join(scratch_gdb, 'Veg_Density_Weighted')))
-
-    # Create Vegetated Areas (Not Multiprocessing)
-    arcpy.SetProgressorLabel('Refining Building and Vegetation Points: Creating Vegetated Areas...')
-    veg_countours = arcpy.CreateUniqueName(os.path.join(scratch_gdb, 'Veg_Contours'))
-    # Create Contour Polygons of Vegetated Weighted Density
-    arcpy.sa.Contour(veg_density, veg_countours, 2, 0.25, 1, 'CONTOUR_POLYGON', None)
-    # Select Vegetated Areas
-    veg_areas = arcpy.CreateUniqueName(os.path.join(scratch_gdb, 'Vegetated_Areas'))
-    arcpy.analysis.Select(veg_countours, veg_areas, 'ContourMax > 0.25')
-
-    # Classify Building Points as Unclassified by Vegetated Area Features
-    arcpy.SetProgressorLabel('Refining Building and Vegetation Points: Reclassifying Building Points in Vegetated Areas...')
-    classify_features_arg = [f'{veg_areas} 0 0 NO_CHANGE NO_CHANGE NO_CHANGE NO_CHANGE', building_codes, compute_stats]
-    classify_features_args = [[las_file] + classify_features_arg for las_file in input_las_files]
-    with Pool(processes=cpu_ct) as pool:
-        results = pool.starmap(lidarlas.SetLasClassCodesUsingFeaturesFiltered, classify_features_args)
-
-    # Classify Unclassified and Unassigned Points to Vegetation Levels
-    arcpy.SetProgressorLabel('Refining Building and Vegetation Points: Building Points to Vegetation...')
-    height_arg = ['GROUND', height_classification, noise_method]
-    height_args = [[las_file] + height_arg + global_args for las_file in input_las_files]
-    with Pool(processes=cpu_ct) as pool:
-        results = pool.starmap(lidarlas.ClassifyLasByHeight, height_args)
-
-# Flag Noise Points as Withheld
-if noise_method:
-    arcpy.SetProgressorLabel('Flagging Noise Points as Withheld...')
-    change_codes_arg = ['7 7 NO_CHANGE NO_CHANGE SET NO_CHANGE;18 18 NO_CHANGE NO_CHANGE SET NO_CHANGE']
-    change_codes_args = [[las_file] + change_codes_arg + global_args for las_file in input_las_files]
-    with Pool(processes=cpu_ct) as pool:
-        results = pool.starmap(lidarlas.ChangeLasClassCodes, change_codes_args)
-
-arcpy.SetProgressorLabel('Changing Never Classified Points to Unassigned...')
-change_codes_arg = ['0 1 NO_CHANGE NO_CHANGE SET NO_CHANGE']
-change_codes_args = [[las_file] + change_codes_arg + global_args for las_file in input_las_files]
-with Pool(processes=cpu_ct) as pool:
-        results = pool.starmap(lidarlas.ChangeLasClassCodes, change_codes_args)
 
 # Create Output LAS Dataset
 if out_las_dataset:
--- Scripts/ClassifyPAMAPLASDataset_Validation.py
+++ Scripts/ClassifyPAMAPLASDataset_Validation.py
@@ -15,21 +15,8 @@ class ToolValidator(object):
         # Include Subfolders: Disable at initialization
         self.params[1].enabled = False
         # Ground Classification Options: Disable options at initialization
+        self.params[6].enabled = False
         self.params[7].enabled = False
-        self.params[8].enabled = False
-        # Building Classification Options: Disable options at initialization
-        self.params[10].enabled = False
-        self.params[11].enabled = False
-        self.params[12].enabled = False
-        # Vegetation Classification Options
-        self.params[14].enabled = False
-        self.params[15].enabled = False
-        self.params[16].enabled = False
-        # Noise Classification Options
-        self.params[19].enabled = False
-        self.params[20].enabled = False
-        self.params[21].enabled = False
-        self.params[21].enabled = False
 
     def updateParameters(self):
         """Modify the values and properties of parameters before internal
@@ -63,22 +50,12 @@ class ToolValidator(object):
                 self.params[2].value = in_sr
         
             # Ground Classification Options: Enable if Ground Detection Method is not empty
-            if self.params[6].value:
+            if self.params[5].value:
+                self.params[6].enabled = True
                 self.params[7].enabled = True
-                self.params[8].enabled = True
             else:
+                self.params[6].enabled = False
                 self.params[7].enabled = False
-                self.params[8].enabled = False
-
-            # Building Classification Options: Enable if Classify Building Points is not empty
-            if self.params[9].value:
-                self.params[10].enabled = True
-                self.params[11].enabled = True
-                self.params[12].enabled = True
-            else:
-                self.params[10].enabled = False
-                self.params[11].enabled = False
-                self.params[12].enabled = False
 
             # Determine Coordinate System Units
             if self.params[2].value:
@@ -90,76 +67,6 @@ class ToolValidator(object):
                     elif (in_sr.VCS and in_sr.VCS.linearUnitName in ['Foot', 'Foot_US']) or in_sr.linearUnitName in ['Foot', 'Foot_US']:
                         linear_unit = 'Feet'
                         area_unit = 'Square Feet'
-
-            # Minimum Rooftop Height: Set default value of 2 Meters (6.562 Feet) based on input spatial reference
-            if self.params[10].enabled == True and not self.params[10].altered:
-                if linear_unit == 'Meters':
-                    self.params[10].value = '2 Meters'
-                elif linear_unit == 'Feet':
-                    self.params[10].value = '6.562 Feet'
-            
-            # Minimum Building Area: Set default value of 10 Square Meters (107.639 Square Feet) based on input spatial reference
-            if self.params[11].enabled == True and not self.params[11].altered:
-                if area_unit == 'Square Meters':
-                    self.params[11].value = '10 SquareMeters'
-                elif area_unit == 'Square Feet':
-                    self.params[11].value = '107.639 SquareFeet'
-            
-            # Vegetation Validation
-            # Exisiting Vegetation Classification Codes: Enable if Vegetation Classification Method is EXISTING_VEGETATION or ALL_VEGETATION
-            if self.params[13].valueAsText in ['EXISTING_VEGETATION', 'ALL_VEGETATION']:
-                self.params[14].enabled = True
-            else:
-                self.params[14].enabled = False
-
-            # Height Classification: Enable if Vegetation Classification Method is not empty, Set default values based on input spatial reference unit of '1 0.5;3 2;4 5;5 65' for meter and '1 1.64;3 6.562;4 16.404;5 213.255' for feet
-            #  Vegetation Area Cell Size: Enable if Vegetation Classification Method is not empty
-            if self.params[13].value:
-                self.params[15].enabled = True
-                self.params[16].enabled = True
-                if self.params[2].value and not self.params[15].altered:
-                    if linear_unit == 'Meters':
-                        self.params[15].value = '1 0.5;3 2;4 5;5 65'
-                    elif linear_unit == 'Feet':
-                        self.params[15].value = '1 1.64;3 6.562;4 16.404;5 213.255'
-                if self.params[2].value and not self.params[16].altered:
-                    if linear_unit == 'Meters':
-                        self.params[16].value = 5
-                    elif linear_unit == 'Feet':
-                        self.params[16].value = 16.404
-            else:
-                self.params[15].enabled = False
-                self.params[16].enabled = False
-
-            # Noise Height From Ground: Enable if Noise Classification is ALL_NOISE or HIGH_NOISE, Set default value based on input spatial reference unit of 65 for meter or 213.255 for feet
-            if self.params[17].valueAsText in ['ALL_NOISE', 'HIGH_NOISE']:
-                self.params[18].enabled = True
-                if self.params[2].value and not self.params[18].altered:
-                    if linear_unit == 'Meters':
-                        self.params[18].value = 65
-                    elif linear_unit == 'Feet':
-                        self.params[18].value = 213.255
-            else:
-                self.params[18].enabled = False
-
-            # Noise Validation
-            if self.params[17].valueAsText == 'ALL_NOISE':
-                self.params[19].enabled = True
-                self.params[20].enabled = True
-                self.params[21].enabled = True
-                # Noise Neighborhood Point Limit: Enable if Noise Classification is ALL_NOISE, Set default value as 25
-                if not self.params[19].altered:
-                    self.params[19].value = 25
-                # Noise Noise Neighborhood Width and Height: Enable if Noise Classification is ALL_NOISE, Set default value based on input spatial reference unit of 10 Meters or 32.808 for feet
-                if self.params[2].value and not self.params[20].altered and not self.params[21].altered:
-                    if linear_unit == 'Meters':
-                        self.params[20].value = self.params[21].value = '10 Meters'
-                    elif linear_unit == 'Feet':
-                        self.params[20].value = self.params[21].value = '32.808 Feet'
-            else:
-                self.params[19].enabled = False
-                self.params[20].enabled = False
-                self.params[21].enabled = False
 
     def updateMessages(self):
         """Modify the messages created by internal validation for each tool
@@ -179,59 +86,6 @@ class ToolValidator(object):
                         self.params[0].setErrorMessage(f'Folder {i} does not contain LAS files. Remove folder from Input Files.')
                 elif i_desc.dataType != 'LasDataset':
                     self.params[0].setIDMessage('ERROR', 814)
-
-        # Classification Feature Classes: Create Error Message if Class Code Filter contians text other than integer between 0 and 255 or a comma
-        if self.params[5].value:
-            class_code_filters = [row[-1] for row in self.params[5].value]
-            classify_features_error = []
-            for f in class_code_filters:
-                # Check for non-integers
-                if f and not all(i.strip().isdigit() for i in f.split(',')):
-                    classify_features_error.append(f"Class Code Filter '{f}' must contain all integers delimited by a comma (,).")
-                # Check for integers between 0 and 255
-                elif f and not all(0 <= int(i.strip()) <= 255 for i in f.split(',')):
-                    classify_features_error.append(f"Class Code Filter '{f}' must contain all integers less than 255.")
-            # Add error message if errors exist
-            if classify_features_error:
-                self.params[5].setErrorMessage('\n'.join(classify_features_error))
-
-        # Value Required Messages
-        value_required = 'Value required.'
-        # Minimum Rooftop Height: Create Error Message if Minimum Rooftop Height is enabled and empty
-        if self.params[10].enabled and not self.params[10].value:
-            self.params[10].setErrorMessage(value_required)
-
-        # Minimum Building Area: Create Error Message if Minimum Building Area is enabled and empty
-        if self.params[11].enabled and not self.params[11].value:
-            self.params[11].setErrorMessage(value_required)
-
-        # Exisiting Vegetation Classification Codes: Create Error Message if Exisiting Vegetation Classification Codes is enabled and empty
-        if self.params[14].enabled and not self.params[14].value:
-            self.params[14].setErrorMessage(value_required)
-
-        # Height Classification: Create Error Message if Height Classification is enabled and empty
-        if self.params[15].enabled and not self.params[15].value:
-            self.params[15].setErrorMessage(value_required)
-        
-        #  Vegetation Area Cell Size: Create Error Message if Vegetation Area Cell Size is enabled and empty if Classify Building Points is not empty or Output Vegetation Areas Feature Class is not empty
-        if self.params[16].enabled and not self.params[16].value and self.params[9].value:
-            self.params[16].setErrorMessage(value_required)
-
-        # Noise Neighborhood Width: Create Error Message if Noise Height From Ground Limit is enabled and empty
-        if self.params[18].enabled and not self.params[18].value:
-            self.params[18].setErrorMessage(value_required)
-        
-        # Noise Neighborhood Point Limit: Create Error Message if Noise Neighborhood Point Limit is enabled and empty
-        if self.params[19].enabled and not self.params[19].value:
-            self.params[19].setErrorMessage(value_required)
-        
-        # Noise Neighborhood Width: Create Error Message if Noise Neighborhood Width is enabled and empty
-        if self.params[20].enabled and not self.params[20].value:
-            self.params[20].setErrorMessage(value_required)
-        
-        # Noise Neighborhood Width: Create Error Message if Noise Neighborhood Width is enabled and empty
-        if self.params[21].enabled and not self.params[21].value:
-            self.params[21].setErrorMessage(value_required)
 
     def isLicensed(self):
         """Set whether tool is licensed to execute."""
